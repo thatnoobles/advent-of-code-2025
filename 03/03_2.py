b4e242686cc96b8a6a50a882980ab95ef7200be4
thatnoobles <danildonchuk@gmail.com>
2025-12-03 21:55:16 -0500

    day 3 part 2

--- 03/03_2.py
+++ 03/03_2.py
@@ -1,23 +1,20 @@
 with open("input.txt", "r") as file:
-    banks = file.read().split("\n")
+    banks = [line for line in file.read().split("\n") if line.strip() != ""]
 
 result = 0
 
 for bank in banks:
-    # The HIGH digit is the first occurrence of the highest
-    # digit in the bank
-    digit_high = 0
-    for i in range(9, 0, -1):
-        try:
-            # If our highest digit is within the last 12 chars of the
-            # string, it doesn't matter b/c the number wouldn't fit
-            digit_high_index = bank[:-12].index(str(i))
-            digit_high = int(bank[digit_high_index])
-            
-            if digit_high_index != len(bank) - 12:
+    digit_indices = []
+    while len(digit_indices) < 12:
+        search_start = 0 if len(digit_indices) == 0 else digit_indices[-1] + 1
+        search_end = len(bank) - 11 + len(digit_indices)
+
+        for i in range(9, 0, -1):
+            try:
+                digit_indices.append(bank[search_start:search_end].index(str(i)) + search_start)
                 break
-        except ValueError:
-            continue
-    print(digit_high)
+            except ValueError:
+                continue
+    result += int("".join([bank[index] for index in digit_indices]))
 
 print(result)
